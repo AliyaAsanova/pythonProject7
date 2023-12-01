7d6d5e6834a27b4ef0336c37e5319ecf7a36613e
AliyaAsanova <150764706+AliyaAsanova@users.noreply.github.com>
2023-12-01 23:06:38 +0600

    change 1 main.py

--- main.py
+++ main.py
@@ -1,16 +1,69 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
 
+def random_row_col():
+    return random.randint(0, 4)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def player_input():
+    while True:
+        try:
+            row = int(input("Enter a row (0-4): "))
+            col = int(input("Enter a column (0-4): "))
+            if 0 <= row <= 4 and 0 <= col <= 4:
+                return row, col
+            else:
+                print("Invalid input. Please enter values between 0 and 4.")
+        except ValueError:
+            print("Invalid input. Please enter numeric values.")
 
+def main():
+    print("Welcome to Sea Battle Game!")
+    
+    # Initialize the board
+    board = [["O"] * 5 for _ in range(5)]
+    player_board = [["O"] * 5 for _ in range(5)]
+    
+    # Place player's ship
+    print_board(player_board)
+    print("Place your ship:")
+    player_ship_row, player_ship_col = player_input()
+    player_board[player_ship_row][player_ship_col] = "S"
+    
+    # Place computer's ship
+    comp_ship_row, comp_ship_col = random_row_col(), random_row_col()
+    
+    print("\nPlayer's Board:")
+    print_board(player_board)
+    
+    # Game loop
+    while True:
+        print("\nYour Turn!")
+        print_board(board)
+        guess_row, guess_col = player_input()
+        
+        if guess_row == comp_ship_row and guess_col == comp_ship_col:
+            print("Congratulations! You sank the computer's ship. You win!")
+            break
+        elif board[guess_row][guess_col] == "X":
+            print("You already guessed that one. Try again.")
+        else:
+            print("You missed!")
+            board[guess_row][guess_col] = "X"
+        
+        print("\nComputer's Turn!")
+        comp_guess_row, comp_guess_col = random_row_col(), random_row_col()
+        
+        if comp_guess_row == player_ship_row and comp_guess_col == player_ship_col:
+            print("Oh no! The computer sank your ship. You lose!")
+            break
+        elif board[comp_guess_row][comp_guess_col] == "X":
+            print("The computer already guessed that one. It tries again.")
+        else:
+            print("The computer missed!")
+            board[comp_guess_row][comp_guess_col] = "X"
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
